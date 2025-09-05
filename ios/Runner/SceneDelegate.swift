7b715f03f7ec45913e35fe1904714a3c4efe4af7
Alireza-Ehiaei <78437701+Alireza-Ehiaei@users.noreply.github.com>
2025-09-05 13:24:52 +0400

    Update SceneDelegate.swift
    ensuring the Flutter module is correctly set up and reverting to a standard SceneDelegate implementation that doesn’t rely on FlutterSceneDelegate

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -1,5 +1,18 @@
 import UIKit
 import Flutter
 
-class SceneDelegate: FlutterSceneDelegate {
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = scene as? UIWindowScene else { return }
+    let window = UIWindow(windowScene: windowScene)
+    let flutterEngine = FlutterEngine(name: "io.flutter")
+    flutterEngine.run()
+    GeneratedPluginRegistrant.register(with: flutterEngine)
+    let controller = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+    window.rootViewController = controller
+    self.window = window
+    window.makeKeyAndVisible()
+  }
 }
