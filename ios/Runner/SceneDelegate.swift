3029734f32361bde4200fdbdc62893ddc7022dba
Alireza-Ehiaei <78437701+Alireza-Ehiaei@users.noreply.github.com>
2025-09-05 13:05:16 +0400

    Update SceneDelegate.swift
    FlutterSceneDelegate handles all Flutter engine initialization and view controller setup

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -2,14 +2,4 @@ import UIKit
 import Flutter
 
 class SceneDelegate: FlutterSceneDelegate {
-  override func scene(
-    _ scene: UIScene,
-    willConnectTo session: UISceneSession,
-    options connectionOptions: UIScene.ConnectionOptions
-  ) {
-    guard let windowScene = scene as? UIWindowScene else { return }
-    let window = UIWindow(windowScene: windowScene)
-    self.window = window
-    window.makeKeyAndVisible()
-  }
 }
